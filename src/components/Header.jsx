bc7a93c64452b98fc305c46361961ac80bfc4a60
Davi Cruz <cruzdavi001@gmail.com>
2025-02-12 11:56:28 -0300

    Criando routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logoSpotify from "../assets/logo/spotify-logo.png";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logoSpotify} alt="Logo Spotify" />
+      <Link className="header__link" to="/">
+        <h1>Spotify</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
+
+const Main = () => {
+  return (
+    <div className="main">
+      {/* ItemList de artistas */}
+      <ItemList
+        title="Artistas"
+        items={5}
+        itemsArray={artistArray}
+        path="/artists"
+        idPath="/artist"
+      />
+      {/* ItemList de Musicas */}
+      <ItemList
+        title="Músicas"
+        items={10}
+        itemsArray={songsArray}
+        path="/songs"
+        idPath="/song"
+      />
+    </div>
+  );
+};
+
+export default Main;
