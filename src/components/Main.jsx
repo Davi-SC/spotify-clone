3b02c637f786f8301cd4d2f201e55f373c603e0e
Davi Cruz <cruzdavi001@gmail.com>
2025-02-12 23:07:49 -0300

    Telas de musica e artistas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,25 +2,34 @@ import ItemList from "./ItemList";
 import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
-const Main = () => {
+const Main = ({ type }) => {
   return (
     <div className="main">
       {/* ItemList de artistas */}
-      <ItemList
-        title="Artistas"
-        items={5}
-        itemsArray={artistArray}
-        path="/artists"
-        idPath="/artist"
-      />
+      {type === "artists" || type === undefined ? (
+        <ItemList
+          title="Artistas"
+          items={5}
+          itemsArray={artistArray}
+          path="/artists"
+          idPath="/artist"
+        />
+      ) : (
+        <></>
+      )}
+
       {/* ItemList de Musicas */}
-      <ItemList
-        title="Músicas"
-        items={10}
-        itemsArray={songsArray}
-        path="/songs"
-        idPath="/song"
-      />
+      {type === "songs" || type === undefined ? (
+        <ItemList
+          title="Músicas"
+          items={10}
+          itemsArray={songsArray}
+          path="/songs"
+          idPath="/song"
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
